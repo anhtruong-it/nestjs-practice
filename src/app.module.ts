bf051e6d78d1aac05bb6307ffdfb5f0513122529
Anh Truong Nguyen <truongnguyen4042@gmail.com>
2023-11-01 23:21:23 +1000

    create mysql database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EventsController } from './events.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Event } from './event.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'admin',
+      database: '1',
+      entities: [Event],
+      synchronize: true, 
+    }),
+    TypeOrmModule.forFeature([Event]),
+  ],
   controllers: [AppController, EventsController],
   providers: [AppService],
 })
--- src/event.entity.ts
+++ src/event.entity.ts
@@ -1,8 +1,21 @@
 /* eslint-disable prettier/prettier */
+// eslint-disable-next-line prettier/prettier
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
 export class Event {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   name: string;
+
+  @Column()
   description: string;
+
+  @Column()
   when: Date;
+
+  @Column()
   address: string;
 }
