3b72f55fe56e36b3b0fbde674420799c34ed981e
Anh Truong Nguyen <truongnguyen4042@gmail.com>
2023-11-07 21:46:02 +1000

    valiationpipe

--- src/create-event.dto.ts
+++ src/create-event.dto.ts
@@ -1,7 +1,15 @@
 /* eslint-disable prettier/prettier */
+import { IsDateString, IsString, Length } from "class-validator";
+
+/* eslint-disable prettier/prettier */
 export class CreateEventDto {
+  @IsString()
+  @Length(5, 255, {message: 'The name length is wrong'})
   name: string;
+  @Length(5, 255)
   description: string;
+  @IsDateString()
   when: string;
+  @Length(5, 255)
   address: string;
 }
